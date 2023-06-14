f02d699db14ba1f8cee70e18f2638e97fe44ef3a
Oluwafemi <femio82@gmail.com>
2023-06-14 16:44:02 +0100

    feat:Structure_app

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,25 @@
-package mai
+package main
 
 import "fmt"
 
+type App struct{}
+
+func (app *App) Run() error {
+	fmt.Println("Seetiing up app")
+
+	return nil
+}
 func main() {
 	fmt.Println("GO REST API")
+	app := App{}
+
+	if err := app.Run(); err != nil {
+		{
+			fmt.Println("Error starting the APi")
+			fmt.Println(err)
+		}
+	}
+
 }
+
+//App - the struct which cotains thinsg like pointers to db connection
